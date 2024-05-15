6f1da8a6147f6a68da1db7f614440e40152052d3
Tomojit123 <102011687+Tomojit123@users.noreply.github.com>
2024-05-16 01:53:23 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,143 +1,184 @@
-// Function to create star rating
-function createStar(ratings) {
-    const starContainer = document.createElement('h3');
-    ratings = Math.min(ratings, 5);
-    for (let i = 0; i < ratings; i++) {
-        const starSpan = document.createElement('span');
-        starSpan.textContent = "⭐";
-        starContainer.appendChild(starSpan);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magazine - Your Ultimate Source for Insightful Articles</title>
+<link rel="canonical" href="https://tomojit123.github.io/Book-reference-website/Components/cart.html">
+<meta name="description" content="Welcome to Magazine, your go-to for insightful articles on various topics. Join us for learning and empowerment!">
+
+<!-- Keywords related to your magazine content -->
+<meta name="keywords" content="magazine, articles, insights, books, literature, reading, reviews, recommendations, fiction, non-fiction, genres, authors, publishing, bookworm, bibliophile">
+
+<!-- Open Graph Tags -->
+<meta property="og:type" content="website">
+<meta property="og:title" content="Magazine - Your Ultimate Source for Insightful Articles">
+<meta property="og:description" content="Welcome to Magazine, your go-to for insightful articles on various topics. Join us for learning and empowerment!">
+<meta property="og:url" content="https://tomojit123.github.io/Book-reference-website/Components/cart.html">
+<meta property="og:image" content="https://i.pinimg.com/736x/82/d3/29/82d329c6b8c4141e3172456815ab6621.jpg">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta property="og:site_name" content="Magazine">
+
+<!-- Apple Touch Icon -->
+<link rel="apple-touch-icon" sizes="180x180" href="https://i.pinimg.com/736x/82/d3/29/82d329c6b8c4141e3172456815ab6621.jpg">
+
+<!-- Favicon -->
+<link rel="icon" type="image/jpg" sizes="32x32" href="https://i.pinimg.com/736x/82/d3/29/82d329c6b8c4141e3172456815ab6621.jpg">
+<link rel="icon" type="image/jpg" sizes="16x16" href="https://i.pinimg.com/736x/82/d3/29/82d329c6b8c4141e3172456815ab6621.jpg">
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+
+<!-- Theme Color -->
+<meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="../Style/styles.css">
+</head>
+<style>
+    body{
+        background-color: rgba(64, 148, 223, 0.103);
+        margin: 3% 6% 10px 6%;
+        padding: 0;
+        font-family: 'Poppins', sans-serif;
     }
-    return starContainer;
-}
-
-
-const showSuccessToast = (title,result,color) => {
-    Swal.fire({
-        icon: result,
-        title: title,
-        toast: true,
-        iconColor:color,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 1000,
-        timerProgressBar: true
-    })
-};
-
-async function myCart() {
-    const email = localStorage.getItem('loggedInUserEmail');
-    console.log(email);
-    try {
-        const response = await fetch(`https://book-app-backend-1.onrender.com/search-book/?email=${email}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error("Failed to fetch cart items");
+    #my-cart {
+        display: flex;
+        flex-direction: row;
+        gap: 2em;
+        flex-wrap: wrap;
+        overflow-x: auto;
+        margin-left: 5%;
+    }
+    .card {
+        flex: 0 0 calc(15%);
+        margin-bottom: 1em;
+    }
+    @media screen and (max-width:768px) {
+        .nav {
+            display: none;
         }
 
-        const data = await response.json();
-        if (data.success) {
-            const userCart = document.getElementById('my-cart');
-            userCart.innerHTML = "";
-            data.data.forEach(card => {
-                if (card) {
-                    const div = document.createElement('div');
-                    div.classList.add('card');
-
-                    div.innerHTML = `
-                        <img src=${card.imageUrl}>
-                        <h4>${card.title}</h4>
-                        <p>${card.writer}</p>
-                        <p>${card.details}</p>
-                        <h3></h3>
-                        <h3>₹ ${card.price}</h3>
-                        <button data-card-id=${card.id}>Remove</button>
-
-                    `;
-                    const starContainer = div.querySelector('h3');
-                    starContainer.appendChild(createStar(card.ratings));
-
-                    userCart.appendChild(div);
-
-                }
-            });
-        } else {
-            const title = "You don't have any books in your cart...";
-            const result = 'error';
-            color='red'
-            showSuccessToast(title,result,color);
+        .app-logo {
+            display: none;
         }
-        addEventListeners();
-    } catch (error) {
-        console.error('Internal server error:', error.message);
-        alert("Internal server error");
     }
-}
 
-// Function to add event listeners to buttons
-function addEventListeners() {
-    document.querySelectorAll('.card button').forEach(button => {
-        button.addEventListener('click', function () {
-            const cardId = this.dataset.cardId;
-            const loggedInUserEmail = localStorage.getItem('loggedInUserEmail');
-            deleteCart(cardId, loggedInUserEmail);
-        });
-    });
-}
-
-async function deleteCart(cardId, email) {
-    try {
-        const result = await Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            iconColor: "red",
-            confirmButtonColor: "#05fb4b",
-            cancelButtonColor: "red",
-            confirmButtonText: "Yes, delete it!"
-        });
-
-        if (result.isConfirmed) {
-            const response = await fetch("https://book-app-backend-1.onrender.com/delete-cart", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ card_Id: cardId, email: email }),
-            });
-
-            if (!response.ok) {
-                throw new Error("Failed to delete from cart");
-            }
-
-            const data = await response.json();
-
-            await Swal.fire({
-                title: "Deleted!",
-                text: "Your file has been deleted.",
-                icon: "success",
-                iconColor: "#05fb4b",
-                confirmButtonColor: "#05fb4b",
-            });
-
-            // Redirect after successful deletion
-            window.location.href = 'https://tomojit123.github.io/Book-reference-website/Components/cart.html';
+    @media screen and (min-width:768px) {
+        .mobile-nav {
+            display: none;
         }
-    } catch (error) {
-        console.error('Error deleting:', error);
-        await Swal.fire({
-            title: "Error",
-            text: "Failed to delete the file.",
-            icon: "error",
-            iconColor: "red",
-        });
+
+        .logo {
+            display: none;
+        }
     }
-}
 
-
-myCart();
+    .nav-menu li {
+        margin-bottom: 3px;
+    }
+</style>
+<body>
+    <div class="nav">
+        <div class="app-logo">
+            <img src="https://i.pinimg.com/736x/82/d3/29/82d329c6b8c4141e3172456815ab6621.jpg" alt="logo/img">
+            <h4>Book App</h4>
+        </div>
+        <div class="nav-item">
+            <ul>
+                <a href="https://tomojit123.github.io/Book-reference-website/Pages/index.html">
+                    <li>Home</li>
+                </a>
+                <a href="https://tomojit123.github.io/Book-reference-website/Pages/about.html">
+                    <li>About</li>
+                </a>
+                <a href="https://tomojit123.github.io/Book-reference-website/Pages/terms.html">
+                    <li>Terms</li>
+                </a>
+                <a href="https://tomojit123.github.io/Book-reference-website/Pages/contactUs.html">
+                    <li>Contact Us</li>
+                </a>
+            </ul>
+        </div>
+        <div class="user">
+            <div class="cart">
+                <a style="text-decoration: none;"
+                    href="https://tomojit123.github.io/Book-reference-website/Components/cart.html"><i
+                        class='bx bx-cart'
+                        style="display: flex; align-items:center; justify-content:center; font-size:30px;color:black"></i></a>
+            </div>
+            <div class="user-box" id="user-box">
+                <div onclick="showBox()" class="user-acc">
+                    <img src="https://tse2.mm.bing.net/th?id=OIP.cCYOz6thNtGtDnD0BYfkLAHaEK&pid=Api&P=0&h=180"
+                        alt="userProfile">
+                </div>
+                <div class="box" id="box" style="padding:2px; border-radius:3px;display:none;">
+                    <ul style="list-style: none;cursor:pointer; padding-left:5px;">
+                        <a id="sign-in-a" href="https://tomojit123.github.io/Book-reference-website/Pages/login.html" style="text-decoration: none;">
+                            <li>Login</li>
+                        </a>
+                        <a id="sign-up-a" href="https://tomojit123.github.io/Book-reference-website/Pages/register.html"
+                            style="text-decoration: none;">
+                            <li>Sign Up</li>
+                        </a>
+                        <a id="log-out-a" onclick="show()" style="text-decoration: none;">
+                            <li>Log Out</li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Mobile screen navigation -->
+    <div>
+        <div style="display: flex; align-items: center; justify-content:space-between">
+            <div class="logo">
+                <img src="https://i.pinimg.com/736x/82/d3/29/82d329c6b8c4141e3172456815ab6621.jpg" alt="logo/img">
+                <h4>Book App</h4>
+            </div>
+            <div class="mobile-nav" style="justify-content:end;">
+                <div class="cart" style="display: flex;align-items:center; gap:1em;">
+                    <a style="text-decoration: none;"
+                        href="https://tomojit123.github.io/Book-reference-website/Components/cart.html"><i
+                            class='bx bx-cart'
+                            style="display: flex; align-items:center; justify-content:center; font-size:30px;color:black"></i></a>
+                    <button id="menu" style="border:none; cursor:pointer;" onclick="myNavigation()"><i
+                            class='bx bx-menu' style="font-size:32px;"></i></button>
+                    <button id="cross" style="border:none; cursor:pointer;display:none;" onclick="myCross()"><i
+                            class='bx bx-x' style="font-size:32px"></i></button>
+                </div>
+            </div>
+        </div>
+        <div id="nav-menu" style="width: 100%; background-color:#aeb0eff4; padding-left:3%;display:none;">
+            <ul>
+                <a href="https://tomojit123.github.io/Book-reference-website/Pages/index.html">
+                    <li>Home</li>
+                </a>
+                <a href="https://tomojit123.github.io/Book-reference-website/Pages/about.html">
+                    <li>About</li>
+                </a>
+                <a href="https://tomojit123.github.io/Book-reference-website/Pages/terms.html">
+                    <li>Terms</li>
+                </a>
+                <a href="https://tomojit123.github.io/Book-reference-website/Pages/contactUs.html">
+                    <li>Contact Us</li>
+                </a>
+                <a id="sign-in-mob-a" href="https://tomojit123.github.io/Book-reference-website/Pages/login.html"
+                    style="text-decoration: none;">
+                    <li>Login</li>
+                </a>
+                <a id="sign-up-mob-a" href="https://tomojit123.github.io/Book-reference-website/Pages/register.html"
+                    style="text-decoration: none;">
+                    <li>Sign Up</li>
+                </a>
+                <a id="log-out-mob-a" onclick="show()" style="text-decoration: none;cursor:pointer;">
+                    <li>Log Out</li>
+                </a>
+            </ul>
+        </div>
+    </div>
+    <div id="my-cart"></div>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="../cart.js"></script>
+    <script src="https://tomojit123.github.io/Book-reference-website/logout.js"></script>
+</body>
+</html>
